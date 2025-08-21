427e0ce4594be3b52e07d39a3be088e8b520c74d
KwatMDPhD <kwat.me@icloud.com>
2025-08-21 14:21:27 -0600

    in and ou.

--- src/Human.jl
+++ src/Human.jl
@@ -2,9 +2,9 @@ module Human
 
 const PK = pkgdir(Human)
 
-const IN = joinpath(PK, "input")
+const IN = joinpath(PK, "in")
 
-const OU = joinpath(PK, "output")
+const OU = joinpath(PK, "ou")
 
 # ----------------------------------------------------------------------------------------------- #
 
